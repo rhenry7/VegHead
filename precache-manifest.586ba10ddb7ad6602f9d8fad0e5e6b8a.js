40457bbe48a3ffa03d0d604e4db316dfb033b40e
rhenry <ramoneh94@gmail.com>
2020-10-16 12:37:28 -0400

    Updates

---
+++ precache-manifest.586ba10ddb7ad6602f9d8fad0e5e6b8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9be92dbc743b7baabdd6b3b75666fbd9",
+    "url": "/VegHead/index.html"
+  },
+  {
+    "revision": "872c7c2a4f710ac9ff34",
+    "url": "/VegHead/static/css/main.0d6e46d1.chunk.css"
+  },
+  {
+    "revision": "7e78b2b5cebeaa591d38",
+    "url": "/VegHead/static/js/2.aa19a0f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/VegHead/static/js/2.aa19a0f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "872c7c2a4f710ac9ff34",
+    "url": "/VegHead/static/js/main.2bf6ca34.chunk.js"
+  },
+  {
+    "revision": "5436725518596ce14f61",
+    "url": "/VegHead/static/js/runtime-main.f28493ed.js"
+  },
+  {
+    "revision": "7745e6953b942fd4530cf4436f2914e3",
+    "url": "/VegHead/static/media/bg_search_desktop_green.7745e695.jpg"
+  }
+]);
